07811fa4808e8fbc754a001bb37dfa9a067c9087
Luis Francisco <lrfrancisco17@gemail@example.com>
2026-02-06 14:14:34 -0500

    updating main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,49 @@
-#terraform {
-#  required_version = ">= 1.5.0"
+terraform {
+  required_version = ">= 1.5.0"
 
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "~> 5.0"
-#    }
-#  }
-#}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
 
 provider "aws" {
   region  = "us-east-1"
   profile = "lab"
 }
 
-# Replace this with the actual RHEL 10 AMI for your region
-variable "rhel10_ami_id" {
+variable "instance_type" {
   type        = string
-  description = "RHEL 10 AMI ID"
-  default     = "ami-0ad50334604831820"
+  description = "EC2 instance type"
+  default     = "t3.micro"
 }
 
-resource "aws_instance" "rhel10_t3_micro" {
-  ami           = var.rhel10_ami_id
-  instance_type = "t3.micro"
-
-  tags = {
+variable "tags" {
+  type = map(string)
+  default = {
     Name = "rhel10-t3-micro"
     Env  = "lab"
   }
+}
+
+data "aws_ami" "rhel10" {
+  most_recent = true
+  owners      = ["309956199498"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-10*"]
+  }
+}
+
+resource "aws_instance" "rhel10_t3_micro" {
+  ami           = data.aws_ami.rhel10.id
+  instance_type = var.instance_type
+  tags          = var.tags
+}
+
+output "instance_public_ip" {
+  value = aws_instance.rhel10_t3_micro.public_ip
 }
